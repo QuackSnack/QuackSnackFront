2bd2c195f198925b5113ebc72ecb060085e25a2d
Gregory <gregory.hue135@gmail.com>
2022-05-24 19:29:49 +0200

    main base

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,10 @@
 import { lazy } from "solid-js";
 
-const Navigation = lazy(() => import("./components/Navigation"));
-const Page = lazy(() => import("./components/Page"));
-
 function App() {
   return (
     <div>
       <header>
-        <h1>Pages</h1>
-        <Navigation/>
-        <Page/>
+        <h1>Food Distribution</h1>
       </header>
     </div>
   );

276728c22ae1a10782552e3646a7ba1e4c72ea51
Gregory <gregory.hue135@gmail.com>
2022-05-29 21:18:04 +0200

    Updated: 29-05-2022 21:18:04

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,15 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { Quasar } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(Quasar, { plugins: {}, })
+app.use(PrimeVue)
 app.use(router)
-
 
 app.mount('#app')

313f67abd28a02da07f6142c7e70780eeee8a59d
Gregory <gregory.hue135@gmail.com>
2022-05-29 16:20:19 +0200

    Updated: 29-05-2022 16:20:19

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
@@ -7,6 +8,8 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Quasar, { plugins: {}, })
 app.use(router)
+
 
 app.mount('#app')

5b6134a8a8d04cb60f0045bcdcad08d7632c046d
Gregory <gregory.hue135@gmail.com>
2022-07-26 13:27:03 +0200

    Switching to React cos Vue is weird

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import logo from './QL_Icon.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header >
+      <body>
+      </body>
+    </div>
+  );
+}
+
+export default App;

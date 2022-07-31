19e20c6772737850954487da0138a51d40952de1
Gregory <gregory.hue135@gmail.com>
2022-07-31 21:00:10 +0200

    adding esling and prettier

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+// filename: App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Header</header>
+      <body>Body</body>
+    </div>
+  );
+}
+
+export default App;

405e61479ed22259a4821836202c210b40d14466
Gregory <ghue@DESKTOP-143CVVO.localdomain>
2022-08-24 21:10:46 +0200

    developped on main instead on dev again . . .

--- front/src/component/FoodCard.tsx
+++ front/src/component/FoodCard.tsx
@@ -1,33 +1,36 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Tooltip, Typography } from '@mui/material'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 
 function FoodCard(props: { food: any }) {
   const { food } = props
+  const description =
+    'articles' in food
+      ? food.articles.map((article: any, index: number) => {
+          if (index + 1 === food.articles.length) {
+            return `and ${article.name}.`
+          }
+          return `${article.name}, `
+        })
+      : food.description
 
   return (
     <Card className='food-card'>
       <CardMedia component='img' className='food-card-image' image={`/images/${food.image}`} alt='Image not working' />
       <CardContent>
-        <Typography gutterBottom variant='h6' component='div'>
-          {food.name} | {food.price} €
+        <Typography variant='h6' className='food-card-name' component='div'>
+          {food.name}
         </Typography>
-        {'articles' in food ? (
-          <Typography variant='body2' color='text.secondary'>
-            {food.articles.map((article: any, index: number) => {
-              if (index + 1 === food.articles.length) {
-                return `and ${article.name}.`
-              }
-              return `${article.name}, `
-            })}
+        <Typography variant='h6' className='food-card-price' component='div'>
+          {food.price} €
+        </Typography>
+        <Tooltip title={<Typography variant='subtitle1'>{description}</Typography>} arrow>
+          <Typography variant='body2' className='food-card-description' color='text.secondary'>
+            {description}
           </Typography>
-        ) : (
-          <Typography variant='body2' color='text.secondary'>
-            {food.description}
-          </Typography>
-        )}
+        </Tooltip>
       </CardContent>
     </Card>
   )
--- front/src/component/FoodList.tsx
+++ front/src/component/FoodList.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Stack from '@mui/material/Stack';
+import Stack from '@mui/material/Stack'
 import FoodCard from './FoodCard'
 
 function FoodList(props: { foods: object[] }) {
   const { foods } = props
-
   return (
-    <Stack direction="row" spacing={2} className='food-list'>
-      {foods.map((food) => (
-        <FoodCard food={food}/>
+    <Stack direction='row' spacing={2} className='food-list'>
+      {foods.map((food: any) => (
+        <FoodCard key={food.id} food={food} />
       ))}
     </Stack>
   )

9560395e20e5be3b011212016f1045ebd3820f3a
Gregory <gregory.hue135@gmail.com>
2022-09-22 21:53:45 +0200

    Push: 22-09-2022 21:53:45

--- front/src/component/FoodModal.tsx
+++ front/src/component/FoodModal.tsx
@@ -1,17 +1,16 @@
-import { ReactElement, useState } from 'react'
+import { FormEvent, ReactElement, useState } from 'react'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
-import { Button, Chip, DialogContentText, Divider, Stack } from '@mui/material'
+import { Button, Chip, DialogContentText, Divider, Stack, FormControlLabel, Radio, RadioGroup } from '@mui/material'
 import { useCurrentContext } from '../plugin/context'
 import { Article } from '../interface/Article'
 import { AddCircle, RemoveCircle } from '@mui/icons-material'
 import { Menu } from '../interface/Menu'
 import { Choice } from '../interface/Choice'
-import MenuChoice from './MenuChoice'
 
-function FoodModal(props: { open: boolean; setOpen: React.Dispatch<React.SetStateAction<number>>; food: Article| Menu }): ReactElement {
+function FoodModal(props: { open: boolean; setOpen: React.Dispatch<React.SetStateAction<number>>; food: Article | Menu }): ReactElement {
   const [quantity, setQuantity] = useState(1)
   const { food } = props
   const { open } = props
@@ -26,6 +25,12 @@ function FoodModal(props: { open: boolean; setOpen: React.Dispatch<React.SetStat
     if (quantity > 1) setQuantity(quantity - 1)
   }
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    console.log(event.target)          
+
+  }
+
   return (
     <div>
       <Dialog open={open} onClose={() => setOpen(-1)}>
@@ -34,24 +39,44 @@ function FoodModal(props: { open: boolean; setOpen: React.Dispatch<React.SetStat
           <DialogTitle id="scroll-dialog-title">
             {food.name} | {food.price} €
           </DialogTitle>
+          <DialogContentText>{food.description}</DialogContentText>
           <Stack direction="row" spacing={1}>
             {food.tag.map((tag: { id: number; name: string }, index: number) => (
               <Chip key={index} label={tag.name} />
             ))}
           </Stack>
-          <DialogContent>
-            <DialogContentText>{food.description}</DialogContentText>
-            {Object.prototype.hasOwnProperty.call(food, "choice") ? (
-              <MenuChoice choice={food.choice}/>
-            ) : (null) }
-          </DialogContent>
-          <Divider />
-          <AddCircle onClick={incrementQuantity} />
-          <h5>{quantity}</h5>
-          <RemoveCircle onClick={decrementQuantity} />
-          <DialogActions className="food-modal-buttons">
-            <Button onClick={() => addBasketContent(food, quantity)}>Add to basket</Button>
-          </DialogActions>
+          <form onSubmit={handleSubmit}>
+            <DialogContent>
+              <Divider />
+              {Object.prototype.hasOwnProperty.call(food, 'choice') ? (
+                <div>
+                  {food.choice.map((c: Choice) => {
+                    return (
+                      <div key={c.id}>
+                        <h3>{c.name}</h3>
+                        <RadioGroup key={c.id}>
+                          {c.possibilities.map((possibility) => {
+                            return <FormControlLabel value={possibility.id} control={<Radio required />} key={possibility.id} label={possibility.name} />
+                          })}
+                        </RadioGroup>
+                      </div>
+                    )
+                  })}
+                </div>
+              ) : (
+                <RadioGroup defaultValue={food.id}>
+                  <FormControlLabel value={food.id} control={<Radio required />} label={food.name} />
+                </RadioGroup>
+              )}
+            </DialogContent>
+            <Divider />
+            <DialogActions className="food-modal-buttons">
+              <RemoveCircle onClick={decrementQuantity} />
+              <h5>{quantity}</h5>
+              <AddCircle onClick={incrementQuantity} />
+              <Button type='submit'>Add to basket</Button>
+            </DialogActions>
+          </form>
         </div>
       </Dialog>
     </div>
